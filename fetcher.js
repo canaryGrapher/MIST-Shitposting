78df7d4f364cf48e53f178ba58b353e5ede134dc
Yash Aryan <yasharyan307@outlook.com>
2021-03-30 02:58:29 +0530

    changed UI

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,22 @@
 const templateGenerator = (date, text) => {
-    const temp_data = `<div class="text-message col-12 col-md-6 d-block mx-2 my-4">
-<div class="message-date">
-    <p class="font-weight-bold text-white pl-4">
-        ${date}
-    </p>
-</div>
-<div class="message-body p-2">
-    <p>${text}</p>
-</div>
-</div>`;
+    const temp_data = `
+    <div class="shitpost-container row">
+        <div class="col-1 text-center">
+            <img src="user.png"
+                class="img-fluid rounded-circle user-image-mini mx-auto" alt="mist">
+        </div>
+        <div class="col-11">
+            <div
+                class="card post-card m-0 w-full d-flex flex-column justify-content-start py-0">
+                <p class="text-white mb-0">
+                    MIST &nbsp;&nbsp;<span
+                        class="text-primary">@sudo.mist</span>
+                </p>    
+                <p class="shitpost-text text-white mb-0 pb-0">${text}</p>
+                <p class="mute-white mt-1">${date}</p>
+            </div>
+        </div>
+    </div>`;
     return temp_data;
 }
 
@@ -16,10 +24,10 @@ const getShitPostData = async () => {
     let innerHTML_container = "";
     const shitpostdata = await fetch('https://raw.githubusercontent.com/canaryGrapher/MIST-Shitposting/master/data/shitposts.json');
     const parsedShitposts = await shitpostdata.json()
-    for (let dummy_incremator = 0; dummy_incremator < parsedShitposts.length; dummy_incremator++) {
+    for (let dummy_incremator = parsedShitposts.length-1; dummy_incremator >= 0; dummy_incremator--) {
         innerHTML_container += templateGenerator(parsedShitposts[dummy_incremator].date, parsedShitposts[dummy_incremator].text)
     }
-    document.getElementById("text-holder").innerHTML = innerHTML_container;
+    document.getElementById("post-insertion-target").innerHTML = innerHTML_container;
 }
 
 
